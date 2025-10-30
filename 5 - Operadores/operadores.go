827fd9920fdfc5bfed1a610b21a0af51d5e1114d
gbrreel <gabrielggalloo@gmail.com>
2025-10-29 22:05:36 -0300

    operadores 2

--- 5 - Operadores/operadores.go
+++ 5 - Operadores/operadores.go
@@ -1,5 +1,15 @@
 package main
 
 func main() {
+	soma := 1 + 2
+	subtracao := 2 - 1
+	multiplicacao := 2 * 3
+	divisao := 10 / 2
+	resto := 10 % 3
 
+	println(soma)
+	println(subtracao)
+	println(multiplicacao)
+	println(divisao)
+	println(resto)
 }
